e2e9459ee27566177538b73162457e94bc010739
Andy Liu <andy@madmachine.io>
2022-01-10 10:38:33 +0800

    Add register
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Sources/W5500/W5500.swift
+++ Sources/W5500/W5500.swift
@@ -3,11 +3,8 @@ import SwiftIO
 
 
 final public class W5500 {
-    var text = "Hello, World!"
 
     public init(spi: SPI, csPin: DigitalOut) {
-        socket(0, 0, 0, 0)
-        w5500_register(nil, nil)
+        w5500_register(spi.obj, csPin.obj)
     }
-
 }
